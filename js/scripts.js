e35fd47a649ec41709b2ecd0cd8cd9be29e4d2d5
Stephany Garcia & Haley Harrison <student@epicodus.com>
2015-09-15 16:01:51 -0700

    second spec passes, third spec needs works

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,26 @@ var pigLatinTranslator = function(word) {
   var pigWord = word1 + pigAy;
 
   if (startsWithAVowel(word)) {
-    return pigWord;
-  }
-}
+      return pigWord;
+  } else if (startsWithTwoConsonants(word)) {
+      var pigWordCons = word.substring(2) + word[0] + "ay";
+      return pigWordCons;
+  } else {
+      var consWord = word.substring(1) + word[0] + "ay";
+      return consWord;
+    }
+};
 
 var startsWithAVowel = function(word) {
   var vowels = ["a", "e", "i", "o", "u"];
   var firstLetter = word.slice(0, 1);
   return vowels.indexOf(firstLetter) !== -1;
+};
+
+var startsWithTwoConsonants = function(word) {
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "y"];
+  var twoCons = word.slice(0,2);
+  return consonants.indexOf(twoCons) !== -1;
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,13 @@
-//spec for words that starts with a vowel (add "ay" to the end)
-
 describe('pigLatinTranslator', function() {
   it("adds 'ay' to a word that starts with a vowel", function() {
     expect(pigLatinTranslator("unique")).to.equal("uniqueay");
+  });
+
+  it("takes a single consonant at the beginning of the word and moves it to the end", function() {
+    expect(pigLatinTranslator("coffee")).to.equal("offeecay");
+  });
+
+  it("takes two consonants at the beginning of the word and moves it to the end", function() {
+    expect(pigLatinTranslator("great")).to.equal("eatgray");
   });
 });
