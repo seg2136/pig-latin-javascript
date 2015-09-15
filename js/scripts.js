185327d22d21178505f2cbff649474c00ebfe98b
Stephany Garcia & Haley Harrison <student@epicodus.com>
2015-09-15 14:02:48 -0700

    first spec passes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-var pigLatin = function(word) {
-  if () {
-    return
+var pigLatinTranslator = function(word) {
+  var word1 = word.toLowerCase();
+  var pigAy = "ay";
+  var pigWord = word1 + pigAy;
+
+  if (startsWithAVowel(word)) {
+    return pigWord;
   }
+}
 
-
+var startsWithAVowel = function(word) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var firstLetter = word.slice(0, 1);
+  return vowels.indexOf(firstLetter) !== -1;
 };
+
+$(function() {
+  
+
+});
